bce936b66acde1f9e6c1602d3604d19b4ba292ca
PatrickCelis <geometrydashforce@gmail.com>
2023-01-23 13:15:50 -0500

    Agregué cambios a mi branch!

--- dist/components/user/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controller_1 = require("./controller");
-const userRouter = (0, express_1.Router)();
-userRouter.get("/", controller_1.findAll);
-userRouter.post("/", controller_1.store);
-exports.default = userRouter;
